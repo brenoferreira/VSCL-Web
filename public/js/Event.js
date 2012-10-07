00313626090d948ec47edd1b599adc97f91b7062
Breno Ferreira <bferreira@windowslive.com>
2012-10-07 16:26:31 -0300

    Implemented talks view

--- public/js/Event.js
+++ public/js/Event.js
@@ -14,22 +14,58 @@ var HomeView = Backbone.View.extend({
     }
 });
 
+var CollectionView = Backbone.View.extend({
+    initialize: function(options){
+        this.template = options.template;
+    },
+
+    render: function(){
+        var self = this;
+        this.collection.fetch({
+            success: function(){
+                var data = self.collection.toJSON();
+
+                var templateHtml = self.template.html();
+                var template = Handlebars.compile(templateHtml);
+
+                var html = template({data: data});
+
+                self.$el.html(html);
+            },
+            error: function(){
+
+            }
+        })
+    }
+})
+
 var Router = Backbone.Router.extend({
     initialize: function(options){
         this.homeView = new HomeView({el: $('#content')});
+        this.talksView = new CollectionView({
+            el: $('#content'), 
+            collection: new Talks(),
+            template: $('#talks-template')
+        });
     },
 
     routes: {
+        'palestras': 'renderTalksView',
         'home': 'renderHomeView'
     },
 
     renderHomeView: function(){
         this.homeView.render();
+    },
+
+    renderTalksView: function(){
+        this.talksView.render();
     }
 });
 
 $(function(){
     var router = new Router();
     Backbone.history.start();
-    Backbone.history.navigate('home', {trigger:true});
+    if(!Backbone.history.fragment)
+        router.navigate('home', {trigger: true});
 })
